d32cbc761710a0d695c99e13132a24095e1baf30
Adriel <adriel.fsantos@outlook.com>
2024-05-09 21:05:58 -0300

    feat: Encapsulate Conta class and add constructor

--- src/main/java/models/Conta.java
+++ src/main/java/models/Conta.java
@@ -1,7 +1,35 @@
 package models;
 
 public class Conta {
-    int agencia;
-    int numero;
-    double saldo;
+    private final int agencia;
+    private final int numero;
+    private double saldo;
+
+    public Conta(int agencia, int numero, double saldo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void depositar(double valor) {
+        if (valor <= 0) throw new IllegalArgumentException("O valor de depósito deve ser positivo");
+        this.saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        if (valor > saldo) throw new IllegalArgumentException("Valor superior ao saldo disponível");
+        this.saldo -= valor;
+    }
 }
